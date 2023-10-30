29da626fc0d04343367e0623a0054941c4dea454
lambojac <you@example.com>
2023-10-30 18:03:09 +0100

    first commit

---
+++ backend/config/alllowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    "http://localhost:3001",
+    'https://www.dandrepairshop.com',
+    'https://dandrepairshop.com'
+]
+
+export default allowedOrigins; 
